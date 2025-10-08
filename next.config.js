6f73e4dada5f201c535d29882a5217a1b8735d1f
Alv-P <alvinpaulp08@gmail.com>
2025-10-08 09:05:01 -0400

    this should fix it

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const repoName = 'ald-chess_club-homepage';
+
+module.exports = {
+  // ...existing config...
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  // ...existing config...
+  // If you use images, add: images: { unoptimized: true }
+};
